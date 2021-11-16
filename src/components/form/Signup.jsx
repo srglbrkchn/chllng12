f77ae61c17685a3cd4306bd3bbcd0044defd6468
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-11-16 12:51:44 +0100

    Design terms and condition <p>. Add boxshadow to form.

--- src/components/form/Signup.jsx
+++ src/components/form/Signup.jsx
@@ -8,9 +8,9 @@ function Signup() {
         <div className="form-content">
             <InputField />
             <GreenBtn type="submit" className="green-btn" text="Claim your free trial" name="submit-btn"/>
-            {/* <p>By clicking the button, you are agreeing to our
-                <span>Terms and Services</span>
-            </p> */}
+            <p>By clicking the button, you are agreeing to our 
+                <span> Terms and Services</span>
+            </p>
         </div>
     );}
 
