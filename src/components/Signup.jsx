3c624c4d6595c9a6f94f5f0d8da34b25a44ce714
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-11-16 09:20:14 +0100

    Extract repetative input fields into separate Entry componenet. Use inputInfo.js file and map function to create input fields automatically.

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import Entery from "./Entery";
+import inputInfos from "../inputInfo";
 
 function Signup() {
     return (
         <div>
-            First Name Last Name Email Address Password 
-            <Entery type="text" placeholder="First Name" />
+            {inputInfos.map(inputInfo => {
+                return (<Entery type={inputInfo.type} placeholder={inputInfo.placeholder} />)
+            })}
             <button className="signup-btn">Claim your free trial</button>
-             <p>By clicking the button, you are agreeing to our <span>Terms and Services</span></p>
+            <p>By clicking the button, you are agreeing to our
+                <span>Terms and Services</span>
+            </p>
         </div>
-    );
-}
+    );}
 
-export default Signup;
+    export default Signup;
---
+++ src/inputInfo.js
@@ -0,0 +1,20 @@
+const inputInfo = [
+    {
+        type: "text",
+        placeholder:"First Name"
+    },
+    {
+        type: "text",
+        placeholder: "Last Name"
+    },
+    {
+        type: "email",
+        placeholder: "Email Address"
+    },
+    {
+        type: "password",
+        placeholder: "Password"
+    }
+]
+
+export default inputInfo;
