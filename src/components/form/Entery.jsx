eec0a0395b49deb06240bde4c56969ffad080544
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-11-18 16:35:18 +0100

    Add error icon to input field. Create input-area.

--- src/components/form/Entery.jsx
+++ src/components/form/Entery.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 
-function Entery (props) {
-    return(
-        <input type={props.type} placeholder={props.placeholder} name={props.name} />
+function Entery(props) {
+    return (
+        <div className="input-area">
+            <input type={props.type} placeholder={props.placeholder} name={props.name}/>
+            <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+                <g fill="none" fill-rule="evenodd"><circle fill="#FF7979" cx="12" cy="12" r="12"/><rect fill="#FFF" x="11" y="6" width="2" height="9" rx="1"/><rect fill="#FFF" x="11" y="17" width="2" height="2" rx="1"/></g>
+            </svg>
+        </div>
     );
 }
 
