4d2b09f12a7259ed277d7fa71cad6cb60f138285
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-11-23 13:35:43 +0100

    Extract ErrorSign out of Entery component.

--- src/components/form/Entery.jsx
+++ src/components/form/Entery.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
+import ErrorSign from "./ErrorSign";
 
 function Entery(props) {
     return (
         <div className="input-outer-area">
             <div className="input-area">
-                <input type={props.type} placeholder={props.placeholder} name={props.name} required/>
-                <svg className="err-sign" width="24" height="24" xmlns="http://www.w3.org/2000/svg">
-                    <g fill="none" fill-rule="evenodd"><circle fill="#FF7979" cx="12" cy="12" r="12"/><rect fill="#FFF" x="11" y="6" width="2" height="9" rx="1"/><rect fill="#FFF" x="11" y="17" width="2" height="2" rx="1"/></g>
-                </svg>
+                <input
+                    type={props.type}
+                    placeholder={props.placeholder}
+                    name={props.name}
+                    required/>
+              <ErrorSign />
             </div>
             <p className="error-msg">{props.errMsg}</p>
         </div>
--- src/components/form/Signup.jsx
+++ src/components/form/Signup.jsx
@@ -8,7 +8,7 @@ function Signup() {
     return (
         <div className="form-main" >
             <BlueHeader emphasis="Try it free 7 days " normal=" then $20/mo.thereafter" />
-            <form className="form-content">
+            <form className="form-content" novalidate>
             <InputField />
             <GreenBtn type="submit" className="green-btn" text="Claim your free trial" name="submit-btn"/>
             <p>By clicking the button, you are agreeing to our 
