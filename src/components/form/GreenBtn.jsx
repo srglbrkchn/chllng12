0139320b390e9a9e9594d21421e10c608e478b07
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-11-23 13:50:59 +0100

    Add preventDefault() function to GreenBtn component to discurt default input error message.

--- src/components/form/GreenBtn.jsx
+++ src/components/form/GreenBtn.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function GreenBtn(props) {
     return (
-        <button className={props.className} type={props.type} name = {props.name}>{props.text}</button>
+        <button onClick={event => {event.preventDefault()}} className={props.className} type={props.type} name = {props.name}>{props.text}</button>
     );
 }
 
