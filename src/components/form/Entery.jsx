41f24892235481e037fbfec5df20aedfdf68c5ae
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-12-05 18:47:01 +0100

    Use useState hooks to check validaty of inputs and display error messages incase of errors.

--- src/components/form/Entery.jsx
+++ src/components/form/Entery.jsx
@@ -5,29 +5,60 @@ function Entery(props) {
     const [inputValue,
         setInputValue] = useState("");
 
+    const [err,
+        setErr] = useState(false);
+
     function handleChange(event) {
         const iValue = event.target.value;
         setInputValue(iValue);
     }
 
     function handleOnBlur(event) {
-        console.log(event.target);
+        if (event.target.validity.valueMissing || event.target.validity.typeMismatch) {
+            setErr(true);
+            event.target.placeholder = "";
+        } else {
+            setErr(false);
+        }
     }
+
+    function handleFocus (event) {
+        setErr(false);
+    }
+
+
 
     return (
         <div className="input-outer-area">
-            <div className="input-area">
+            <div
+                className="input-area"
+                style={{
+                border: err && "solid 2px hsl(0, 100%, 74%)"
+            }}>
                 <input
                     type={props.type}
                     placeholder={props.placeholder}
                     name={props.name}
                     onChange={handleChange}
                     onBlur={handleOnBlur}
+                    onFocus={handleFocus}
                     value={inputValue}
+                    style={{
+                    color: err && "hsl(0, 100%, 74%)"
+                }}
                     required/>
-                <ErrorSign visibility=""/>
+                <ErrorSign
+                    visibility={err
+                    ? "visible"
+                    : "hidden"}/>
             </div>
-            <p className="error-msg">{props.errMsg}</p>
+            <p
+                className="error-msg"
+                style={{
+                display: err
+                    ? "block"
+                    : "none"
+            }}>{props.errMsg}</p>
         </div>
     );
 }
